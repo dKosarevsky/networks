604b4dac614e39ac309a34daf00eb2716e8ae5de
kosarevskiydp <kosarevskiydp@pik.ru>
2022-03-08 19:47:00 +0300

    lab_02 part_01 UDP

---
+++ lab_02/src/UDP/client.c
@@ -0,0 +1,43 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "settings.h"
+
+void perror_and_exit(char *s) {
+    perror(s);
+    exit(1);
+}
+
+int main(void) {
+    struct sockaddr_in server_addr;
+    int sock, slen = sizeof(server_addr);
+    char buf[MSG_LEN];
+
+    sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (sock < 0)
+        perror_and_exit("[-] socket");
+
+    memset((char *) &server_addr, 0, sizeof(server_addr)); // заполняем нулями
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SOCK_PORT); // преобразует в сетевой порядок расположения байтов
+
+    if (inet_aton(SRV_IP, &server_addr.sin_addr) == 0) {
+        fprintf(stderr, "[-] inet_aton() failed\n");
+        exit(1);
+    }
+
+    printf("[+] Input decimal number: ");
+    fgets(buf, MSG_LEN, stdin);
+
+    if (sendto(sock, buf, MSG_LEN, 0, (struct sockaddr *) &server_addr, slen) == -1)
+        perror_and_exit("[-] sendto()");
+
+    close(sock);
+    return 0;
+}
---
+++ lab_02/src/UDP/server.c
@@ -0,0 +1,91 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "settings.h"
+
+void perror_and_exit(char *s) {
+    perror(s);
+    exit(1);
+}
+
+int convert_int(int a, int p, char *s) {
+    // a - число в 10-й системе счисления
+    // p - основание системы счисления [2, 30]
+    // s - результат
+    char letters[30] = {"0123456789ABCDEFGHIJKLMNOPQRST"};
+
+    int num = (int) a;
+    int rest = num % p;
+    num /= p;
+
+    if (num == 0) {
+        s[0] = letters[rest];
+        return 1;
+    }
+
+    int k = convert_int(num, p, s);
+    s[k++] = letters[rest];
+    return k;
+}
+
+void convert_and_print(char *b) {
+    int n = atoi(b);
+    char res2[RES_LEN] = {0};
+    convert_int(n, 2, res2);
+
+    char res7[RES_LEN] = {0};
+    convert_int(n, 7, res7);
+
+    printf("decimal: %d\n", n);
+    printf("binary: %s\n", res2);
+    printf("octal: %o\n", n);
+    printf("hexadecimal: %x\n", n);
+    printf("septimal: %s\n\n", res7); // с основанием 7 - номер студента Косаревский по списку группы
+}
+
+
+int main(void) {
+    struct sockaddr_in server_addr, client_addr;
+    int sock, slen = sizeof(client_addr);
+    char buf[MSG_LEN];
+
+    if ((sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+        perror_and_exit("[-] socket");
+    printf("[+] Server started\n");
+
+    memset((char *) &server_addr, 0, sizeof(server_addr));
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SOCK_PORT);
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    if (bind(sock, (struct sockaddr *) &server_addr, sizeof(server_addr)) == -1)
+        perror_and_exit("[-] bind");
+
+    while (1) {
+        socklen_t addr_size;
+        addr_size = sizeof(client_addr);
+        if (recvfrom(sock, buf, MSG_LEN, 0, (struct sockaddr *) &client_addr, &addr_size) == -1)
+            perror_and_exit("[-] recvfrom()");
+
+        printf("[+] Received packet from %s:%d\n\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+
+        convert_and_print(buf);
+
+        printf("[+] Shutdown server? [y/n]: ");
+        char buf_l[MSG_LEN];
+        fgets(buf_l, MSG_LEN, stdin);
+        if (*buf_l == 'y' || *buf_l == 'Y') {
+            printf("[+] Server shutdown...\n");
+            break;
+        }
+    }
+
+    close(sock);
+    return 0;
+}
